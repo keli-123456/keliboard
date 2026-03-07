02e3a416b2e9e47102a20bf584720d891fd4012d
keliboard <keli-123456@users.noreply.github.com>
2026-03-07 12:37:11 +0800

    feat: expose commission mode in user config

--- app/Http/Controllers/V1/User/CommController.php
+++ app/Http/Controllers/V1/User/CommController.php
@@ -23,7 +23,8 @@ class CommController extends Controller
             'commission_distribution_enable' => (int)admin_setting('commission_distribution_enable', 0),
             'commission_distribution_l1' => admin_setting('commission_distribution_l1'),
             'commission_distribution_l2' => admin_setting('commission_distribution_l2'),
-            'commission_distribution_l3' => admin_setting('commission_distribution_l3')
+            'commission_distribution_l3' => admin_setting('commission_distribution_l3'),
+            'commission_first_time_enable' => (int)admin_setting('commission_first_time_enable', 1),
         ];
         return $this->success($data);
     }
