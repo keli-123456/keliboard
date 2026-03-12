692c411cace9606a0197fe4c68287ede0569fbfe
keliboard <keli-123456@users.noreply.github.com>
2026-03-12 11:26:43 +0800

    fix: prevent stale xadmin chunk 404

--- resources/views/admin_xboard.blade.php
+++ resources/views/admin_xboard.blade.php
@@ -5,6 +5,11 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>{{ $title }}</title>
+  @php
+    $adminAssetBase = public_path('assets/admin-xboard/assets');
+    $adminIndexJsVersion = is_file($adminAssetBase . '/index.js') ? filemtime($adminAssetBase . '/index.js') : time();
+    $adminIndexCssVersion = is_file($adminAssetBase . '/index.css') ? filemtime($adminAssetBase . '/index.css') : time();
+  @endphp
   <script>
     window.settings = {
       base_url: "/",
@@ -14,8 +19,8 @@
       secure_path: "{{ $secure_path }}",
     };
   </script>
-  <script type="module" crossorigin src="/assets/admin-xboard/assets/index.js"></script>
-  <link rel="stylesheet" crossorigin href="/assets/admin-xboard/assets/index.css" />
+  <script type="module" crossorigin src="/assets/admin-xboard/assets/index.js?v={{ $adminIndexJsVersion }}"></script>
+  <link rel="stylesheet" crossorigin href="/assets/admin-xboard/assets/index.css?v={{ $adminIndexCssVersion }}" />
 </head>
 
 <body>
@@ -23,4 +28,3 @@
 </body>
 
 </html>
-
